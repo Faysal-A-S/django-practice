b35324fb967ff2ad98721ffa8792092a8a7eea15
Faysal-A-S <faysala1655@gmail.com>
2025-07-27 17:01:01 +0600

    feat: product update serializer add

--- api/serializers.py
+++ api/serializers.py
@@ -17,19 +17,35 @@ class UserSerializer(serializers.ModelSerializer):
         return user
 
 
-class BrandSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Brand
-        fields = ['id', 'name', "create_at", "updated_at"]
-
-
 class ProductSerializer(serializers.ModelSerializer):
-    brand = BrandSerializer(read_only=True)
+    brand = serializers.PrimaryKeyRelatedField(
+        queryset=Brand.objects.all())
 
     class Meta:
         model = Product
-        fields = ['id', 'name', 'brand', 'description',
-                  'price', 'created_at', 'updated_at']
+        fields = "__all__"
+
+
+class UpdateProductSerializer(serializers.ModelSerializer):
+    brand = serializers.PrimaryKeyRelatedField(
+        queryset=Brand.objects.all(), required=False)
+
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for fields in self.fields.values():
+            fields.required = False
+
+
+class BrandSerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Brand
+        fields = "__all__"
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
