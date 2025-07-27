7bc3de4c8db6277f0dee2facee03b37b23d2c8fb
Faysal-A-S <faysala1655@gmail.com>
2025-07-27 17:01:54 +0600

    feat: product and brand api view add

--- api/views.py
+++ api/views.py
@@ -1,8 +1,8 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
-from .models import User
-from .serializers import UserSerializer, MyTokenObtainPairSerializer
+from .models import User, Brand, Product
+from .serializers import UserSerializer, MyTokenObtainPairSerializer, BrandSerializer, ProductSerializer, UpdateProductSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
 
@@ -30,3 +30,63 @@ def login(request):
     if serializer.is_valid():
         return Response(serializer.validated_data, status=status.HTTP_200_OK)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["POST"])
+def createBrand(request):
+    serializer = BrandSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET"])
+def getBrand(request):
+    brands = Brand.objects.all()
+    serializer = BrandSerializer(brands, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(["GET"])
+def getProduct(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(["POST"])
+def createProduct(request):
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["PUT"])
+def updateProduct(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        return Response({"error": "product not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    serializer = UpdateProductSerializer(
+        product, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["DELETE"])
+def deleteProduct(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+
+    except Product.DoesNotExist:
+        return Response({"errors": "Product Doesn't exists"}, status=status.HTTP_404_NOT_FOUND)
+
+    product.delete()
+    return Response({"message": "Product deleted"}, status=status.HTTP_204_NO_CONTENT)
