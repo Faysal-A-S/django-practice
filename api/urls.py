4b29a4ffe9651c1df06054d42296ffd19ee369fd
Faysal-A-S <faysala1655@gmail.com>
2025-07-28 16:36:09 +0600

    feat: url update for class based api

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
-from .views import getUsers, createUsers, login, createBrand, getBrand, getProduct, createProduct, updateProduct, deleteProduct
+from .views import UserAPIView, UserLogin, ProductAPIView, BrandAPIView, SingleProductView
 
 urlpatterns = [
-    path("users/", getUsers, name="get_users"),
-    path("users/create/", createUsers, name="create_user"),
-    path("users/login/", login, name="login"),
-    path("brands/create/", createBrand, name="create_brand"),
-    path("brands/", getBrand, name="get_brands"),
-    path("products/", getProduct, name="get_products"),
-    path("products/create/", createProduct, name="create_product"),
-    path("products/update/<int:pk>/", updateProduct, name="update_product"),
-    path("products/delete/<int:pk>/", deleteProduct, name="delete_product")
+    path("users/", UserAPIView.as_view(), name="users"),
+    # path("users/create/", createUsers, name="create_user"),
+    path("users/login/", UserLogin.as_view(), name="login"),
+    # path("brands/create/", createBrand, name="create_brand"),
+    path("brands/", BrandAPIView.as_view(), name="brands"),
+    path("products/", ProductAPIView.as_view(), name="products"),
+    path("products/<int:pk>", SingleProductView.as_view(), name="single_product")
+    # path("products/create/", createProduct, name="create_product"),
+    # path("products/update/<int:pk>/", updateProduct, name="update_product"),
+    # path("products/delete/<int:pk>/", deleteProduct, name="delete_product")
 ]
