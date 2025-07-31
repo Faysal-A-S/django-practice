d68267e275a774baa692dd68cedfb1d95ed9ff61
Faysal-A-S <faysala1655@gmail.com>
2025-07-31 14:46:16 +0600

    feat: role add

--- api/models.py
+++ api/models.py
@@ -3,29 +3,39 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password, **extra_fields):
+    def create_user(self, email, password, role, **extra_fields):
 
         if not email:
             raise ValueError("Email is mandatory")
 
         if not password:
             raise ValueError("Password is mandatory")
+        if not role:
+            raise ValueError("role is mandatory")
 
         extra_fields.setdefault('is_active', True)
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        role = self.normalize_email(role)
+        user = self.model(email=email, role=role, ** extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
 
 class User(AbstractBaseUser):
+    ROLE_CHOICES = (
+        ("admin", "Admin"),
+        ("moderator", "Moderator"),
+        ("user", "User"),
+    )
     username = models.CharField(max_length=150, unique=True)
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=30, blank=True)
     last_name = models.CharField(max_length=30, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    role = models.CharField(
+        max_length=30, choices=ROLE_CHOICES)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
     objects = UserManager()
@@ -34,23 +44,23 @@ class User(AbstractBaseUser):
         return self.email
 
 
-class Brand(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+# class Brand(models.Model):
+#     name = models.CharField(max_length=255, unique=True)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
 
-class Product(models.Model):
-    name = models.CharField(max_length=255)
-    brand = models.ForeignKey(
-        Brand, related_name='products', on_delete=models.CASCADE)
-    description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+# class Product(models.Model):
+#     name = models.CharField(max_length=255)
+#     brand = models.ForeignKey(
+#         Brand, related_name='products', on_delete=models.CASCADE)
+#     description = models.TextField(blank=True)
+#     price = models.DecimalField(max_digits=10, decimal_places=2)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
