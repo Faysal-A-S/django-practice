c4ee715d0a2b14db3a923fecb04a9a27f13e6af4
Faysal-A-S <faysala1655@gmail.com>
2025-07-31 14:48:09 +0600

    feat: role based authentication add

--- api/views.py
+++ api/views.py
@@ -1,109 +1,27 @@
-from rest_framework.decorators import api_view, permission_classes
+
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from .models import User, Brand, Product
-from .serializers import UserSerializer, MyTokenObtainPairSerializer, BrandSerializer, ProductSerializer, UpdateProductSerializer
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import User
+from .serializers import UserSerializer, MyTokenObtainPairSerializer, UpdateUserSerializer
 
-
-# @api_view(["GET"])
-# @permission_classes([IsAuthenticated])
-# def getUsers(request):
-#     users = User.objects.all()
-#     serializer = UserSerializer(users, many=True)
-#     return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-# @api_view(["POST"])
-# def createUsers(request):
-#     serializer = UserSerializer(data=request.data)
-#     if serializer.is_valid():
-#         serializer.save()
-#         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# @api_view(["POST"])
-# def login(request):
-#     serializer = MyTokenObtainPairSerializer(data=request.data)
-#     if serializer.is_valid():
-#         return Response(serializer.validated_data, status=status.HTTP_200_OK)
-#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# @api_view(["POST"])
-# def createBrand(request):
-#     serializer = BrandSerializer(data=request.data)
-#     if serializer.is_valid():
-#         serializer.save()
-#         return Response(serializer.data, status=status.HTTP_201_CREATED)
-#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# @api_view(["GET"])
-# def getBrand(request):
-#     brands = Brand.objects.all()
-#     serializer = BrandSerializer(brands, many=True)
-#     return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-# @api_view(["GET"])
-# def getProduct(request):
-#     products = Product.objects.all()
-#     serializer = ProductSerializer(products, many=True)
-#     return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-# @api_view(["POST"])
-# def createProduct(request):
-#     serializer = ProductSerializer(data=request.data)
-#     if serializer.is_valid():
-#         serializer.save()
-#         return Response(serializer.data, status=status.HTTP_201_CREATED)
-#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# @api_view(["PUT"])
-# def updateProduct(request, pk):
-#     try:
-#         product = Product.objects.get(pk=pk)
-#     except Product.DoesNotExist:
-#         return Response({"error": "product not found"}, status=status.HTTP_404_NOT_FOUND)
-
-#     serializer = UpdateProductSerializer(
-#         product, data=request.data, partial=True)
-#     if serializer.is_valid():
-#         serializer.save()
-#         return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
-
-#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# @api_view(["DELETE"])
-# def deleteProduct(request, pk):
-#     try:
-#         product = Product.objects.get(pk=pk)
-
-#     except Product.DoesNotExist:
-#         return Response({"errors": "Product Doesn't exists"}, status=status.HTTP_404_NOT_FOUND)
-
-#     product.delete()
-#     return Response({"message": "Product deleted"}, status=status.HTTP_204_NO_CONTENT)
 
 class UserAPIView(APIView):
-    def get_permissions(self):
-        if self.request.method == "GET":
-            return [IsAuthenticated()]
-        return [AllowAny()]
 
     def get(self, request):
+        if not request.user.is_authenticated:
+            return Response({"errors": "Not authenticated"}, status=status.HTTP_401_UNAUTHORIZED)
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
+        if not request.user.is_authenticated:
+            return Response({"errors": "Not authenticated"}, status=status.HTTP_401_UNAUTHORIZED)
+        if request.user.role not in ["admin", "moderator"]:
+            return Response({"errors": "No permission"}, status=status.HTTP_403_FORBIDDEN)
+        if User.objects.filter(email=request.data.get("email")):
+            return Response({"error": "User already exists"}, status=status.HTTP_400_BAD_REQUEST)
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -112,72 +30,57 @@ class UserAPIView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class SingleUserAPIView(APIView):
+    def patch(self, request, pk):
+        if not request.user.is_authenticated:
+            return Response({"errors": "Not authenticated"}, status=status.HTTP_401_UNAUTHORIZED)
+        if request.user.role not in ["admin", "moderator"]:
+            return Response({"errors": "No permission"}, status=status.HTTP_403_FORBIDDEN)
+        try:
+            user = User.objects.get(pk=pk)
+        except User.DoesNotExist():
+            return Response({"error": "user doesn't exists"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = UpdateUserSerializer(
+            user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk):
+        if not request.user.is_authenticated:
+            return Response({"errors": "Not authenticated"}, status=status.HTTP_401_UNAUTHORIZED)
+        if request.user.role not in ["admin", "moderator"]:
+            return Response({"errors": "No permission"}, status=status.HTTP_403_FORBIDDEN)
+        try:
+            user = User.objects.get(pk=pk)
+        except User.DoesNotExist():
+            return Response({"error": "user doesn't exists"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        if not request.user.is_authenticated:
+            return Response({"errors": "Not authenticated"}, status=status.HTTP_401_UNAUTHORIZED)
+        if request.user.role != "admin":
+            return Response({"errors": "No permission"}, status=status.HTTP_403_FORBIDDEN)
+        user = User.objects.get(pk=pk)
+        if User.DoesNotExist():
+            return Response({"error": "No user found with this data"}, status=status.HTTP_404_NOT_FOUND)
+
+        user.delete()
+        return Response({"message": "user deleted"}, status=status.HTTP_202_ACCEPTED)
+
+
 class UserLogin(APIView):
 
     def post(self, request):
         serializer = MyTokenObtainPairSerializer(data=request.data)
         if serializer.is_valid():
             return Response(serializer.validated_data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class ProductAPIView(APIView):
-    def get(self, request):
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def post(self, request):
-        serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class SingleProductView(APIView):
-    def get(self, request, pk):
-        product = Product.objects.get(pk=pk)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def put(self, request, pk):
-
-        try:
-            product = Product.objects.get(pk=pk)
-        except Product.DoesNotExist:
-            return Response({"error": "product not found"}, status=status.HTTP_404_NOT_FOUND)
-
-        serializer = UpdateProductSerializer(
-            product, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-
-        try:
-            product = Product.objects.get(pk=pk)
-
-        except Product.DoesNotExist:
-            return Response({"errors": "Product Doesn't exists"}, status=status.HTTP_404_NOT_FOUND)
-
-        product.delete()
-        return Response({"message": "Product deleted"}, status=status.HTTP_204_NO_CONTENT)
-
-
-class BrandAPIView(APIView):
-    def get(self, request):
-        brands = Brand.objects.all()
-        serializer = BrandSerializer(brands, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def post(self, request):
-        serializer = BrandSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
