6f7b7968c0e4dd5cac3654441e92fd77d3b43e5e
Faysal-A-S <faysala1655@gmail.com>
2025-07-31 14:47:29 +0600

    feat: unused path remove

--- api/urls.py
+++ api/urls.py
@@ -1,15 +1,10 @@
 from django.urls import path
-from .views import UserAPIView, UserLogin, ProductAPIView, BrandAPIView, SingleProductView
+from .views import UserAPIView, UserLogin,  SingleUserAPIView
 
 urlpatterns = [
-    path("users/", UserAPIView.as_view(), name="users"),
-    # path("users/create/", createUsers, name="create_user"),
-    path("users/login/", UserLogin.as_view(), name="login"),
-    # path("brands/create/", createBrand, name="create_brand"),
-    path("brands/", BrandAPIView.as_view(), name="brands"),
-    path("products/", ProductAPIView.as_view(), name="products"),
-    path("products/<int:pk>", SingleProductView.as_view(), name="single_product")
-    # path("products/create/", createProduct, name="create_product"),
-    # path("products/update/<int:pk>/", updateProduct, name="update_product"),
-    # path("products/delete/<int:pk>/", deleteProduct, name="delete_product")
+    path("", UserAPIView.as_view(), name="users"),
+    path("/<int:pk>", SingleUserAPIView.as_view(), name="user"),
+    path("login/", UserLogin.as_view(), name="login"),
+
+
 ]
