bb7a7b2902421108dea1fb446bccebfade42d305
Faysal-A-S <faysala1655@gmail.com>
2025-08-09 23:23:55 +0600

    feat: urls add

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-
+from .views import ProductAPIView, SingleProductAPIView
 
 urlpatterns = [
+    path("", ProductAPIView.as_view(), name="products"),
+    path("<int:id>", SingleProductAPIView.as_view(), name="single_product")
 ]
