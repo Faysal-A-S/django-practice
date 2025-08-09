f621792b6d57f105cacd47cd5a4edb7627682d66
Faysal-A-S <faysala1655@gmail.com>
2025-08-09 23:23:35 +0600

    feat: custom permission class use

--- inventory/views.py
+++ inventory/views.py
@@ -3,21 +3,24 @@ from rest_framework import status
 from rest_framework.views import APIView
 from .serializers import ProductSerializer, SingleProductSerializer
 from .models import Product
+from api.permissions import IsAdmin, IsAdminOrModerator, IsModerator
+from rest_framework.permissions import IsAuthenticated
 
 
 class ProductAPIView(APIView):
+    def get_permissions(self):
+        if self.request.method == "GET":
+            return [IsAuthenticated()]
+        elif self.request.method == "POST":
+            return [IsAuthenticated(), IsAdminOrModerator()]
+        return super().get_permissions()
+
     def get(self, request):
-        if not request.user.is_authenticated:
-            return Response({"errors": "Not authenticated"}, status=status.HTTP_401_UNAUTHORIZED)
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
-        if not request.user.is_authenticated:
-            return Response({"errors": "Not authenticated"}, status=status.HTTP_401_UNAUTHORIZED)
-        if request.user.role not in ["admin", "moderator"]:
-            return Response({"errors": "No permission"}, status=status.HTTP_403_FORBIDDEN)
         serializer = ProductSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -27,18 +30,21 @@ class ProductAPIView(APIView):
 
 
 class SingleProductAPIView(APIView):
+    def get_permissions(self):
+        if self.request.method == "GET":
+            return [IsAuthenticated()]
+        elif self.request.method in ["POST", "PATCH", "PUT"]:
+            return [IsAuthenticated(), IsAdminOrModerator()]
+        elif self.request.method == "DELETE":
+            return [IsAuthenticated(), IsAdmin()]
+        return super().get_permissions()
+
     def get(self, request, pk):
-        if not request.user.is_authenticated:
-            return Response({"errors": "Not authenticated"}, status=status.HTTP_401_UNAUTHORIZED)
         products = Product.objects.filter(pk=pk)
         serializer = SingleProductSerializer(products)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def patch(self, request, pk):
-        if not request.user.is_authenticated:
-            return Response({"errors": "Not authenticated"}, status=status.HTTP_401_UNAUTHORIZED)
-        if request.user.role not in ["admin", "moderator"]:
-            return Response({"errors": "No permission"}, status=status.HTTP_403_FORBIDDEN)
         try:
             product = Product.objects.get(pk=pk)
         except Product.DoesNotExist():
@@ -52,10 +58,6 @@ class SingleProductAPIView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def put(self, request, pk):
-        if not request.user.is_authenticated:
-            return Response({"errors": "Not authenticated"}, status=status.HTTP_401_UNAUTHORIZED)
-        if request.user.role not in ["admin", "moderator"]:
-            return Response({"errors": "No permission"}, status=status.HTTP_403_FORBIDDEN)
         try:
             product = Product.objects.get(pk=pk)
         except Product.DoesNotExist():
@@ -68,14 +70,9 @@ class SingleProductAPIView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
-        if not request.user.is_authenticated:
-            return Response({"errors": "Not authenticated"}, status=status.HTTP_401_UNAUTHORIZED)
-        if request.user.role != "admin":
-            return Response({"errors": "No permission"}, status=status.HTTP_403_FORBIDDEN)
         product = Product.objects.get(pk=pk)
         if Product.DoesNotExist():
             return Response({"error": "No product found with this data"}, status=status.HTTP_404_NOT_FOUND)
 
         product.delete()
         return Response({"message": "product deleted"}, status=status.HTTP_202_ACCEPTED)
-
