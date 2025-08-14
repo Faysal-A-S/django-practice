63ecb8cdd956348c7c7db67012e504434893ed05
Faysal-A-S <faysala1655@gmail.com>
2025-08-14 17:39:00 +0600

    profile serializer add

--- api/models.py
+++ api/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.conf import settings
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password, role, **extra_fields):
-
+    def create_user(self,  **extra_fields):
+        email = extra_fields.pop("email")
+        password = extra_fields.pop("password")
+        role = extra_fields.pop("role")
         if not email:
             raise ValueError("Email is mandatory")
 
@@ -15,11 +18,19 @@ class UserManager(BaseUserManager):
 
         extra_fields.setdefault('is_active', True)
         email = self.normalize_email(email)
-        role = self.normalize_email(role)
         user = self.model(email=email, role=role, ** extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
+
+
+class Profile(models.Model):
+    profile_picture = models.ImageField(
+        upload_to="profiles/", blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile")
 
 
 class User(AbstractBaseUser):
@@ -42,25 +53,3 @@ class User(AbstractBaseUser):
 
     def __str__(self):
         return self.email
-
-
-# class Brand(models.Model):
-#     name = models.CharField(max_length=255, unique=True)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-
-#     def __str__(self):
-#         return self.name
-
-
-# class Product(models.Model):
-#     name = models.CharField(max_length=255)
-#     brand = models.ForeignKey(
-#         Brand, related_name='products', on_delete=models.CASCADE)
-#     description = models.TextField(blank=True)
-#     price = models.DecimalField(max_digits=10, decimal_places=2)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-
-#     def __str__(self):
-#         return self.name
