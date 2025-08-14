a3747837319d0119a25c8ec645bfecb6a3cd18b3
Faysal-A-S <faysala1655@gmail.com>
2025-08-14 17:40:33 +0600

    feat: single user url add

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from .views import UserAPIView, UserLogin,  SingleUserAPIView
 
 urlpatterns = [
     path("", UserAPIView.as_view(), name="users"),
-    path("/<int:pk>", SingleUserAPIView.as_view(), name="user"),
+    path("<int:pk>", SingleUserAPIView.as_view(), name="user"),
     path("login/", UserLogin.as_view(), name="login"),
 
 
